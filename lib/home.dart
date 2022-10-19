849ffb179674471b7765bd6fba20ad31e762fa7e
BernardoWitkoski <bernardowitkoski@hotmail.com>
2022-10-19 20:11:47 -0300

    Adding maps into view

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+//google maps required imports
+import 'dart:async';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -8,10 +12,22 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  Completer<GoogleMapController> _controller = Completer();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: AppBar(),
+      body: GoogleMap(
+        initialCameraPosition: CameraPosition(
+          zoom: 16,
+          target: LatLng(-27.61310389786854, -52.22349063811818)),
+          onMapCreated: (GoogleMapController controller) {
+            _controller.complete(controller);
+          }
+        ),
+
     );
   }
 }
